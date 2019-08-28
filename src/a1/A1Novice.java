8af35e70da0a7cbd81140cbee1b5b7185c4fcef7
cchow <cchow@UNC-R90N9CMK.home>
2019-08-28 19:52:39 -0400

    Second Commit

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,8 +7,39 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
+		int amtOfCustomers = scan.nextInt();
+		
+		for (int i=0; i<amtOfCustomers; i++) {
+			String fName=scan.next();
+			String lName=scan.next();
+			
+			int amtOfItems = scan.nextInt();
+			
+			double totalItemsCost = 0;
+			
+			for(int b=0; b<amtOfItems; b++) {
+				int itemAmt=scan.nextInt();
+				scan.next();
+				double itemCost=scan.nextDouble();
+				
+				double totalCost= itemAmt * itemCost;
+				
+				totalItemsCost = totalItemsCost + totalCost;
+			}
+			
+			System.out.println(fName.charAt(0) + ". " + lName + ": " + String.format("%.2f", totalItemsCost));
 
-		// Your code follows here.
+		}
+		
+//		for (int i=0; i<amtOfCustomers; i++) {
+//			System.out.println("test");
+//		}
+		
+		scan.close();
+		
+		
+	}
+	static void intScanner (int amount) {
 		
 	}
 }
